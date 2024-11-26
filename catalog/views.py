fa641b0a0a2b8808986ed28d93cafd17187da8de
Veronika Surkova <surkova_nika92@mail.ru>
2024-11-26 19:10:42 +0300

    add writing to file json

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import json
 
 
 def home(request):
@@ -10,5 +11,18 @@ def contacts(request):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
+        to_json = {'name': name,
+                   'phone': phone,
+                   'message': message}
         print(f'{name} ({phone}): {message}')
+        try:
+            with open("contacts_data.json", "r") as json_file:
+                content = json.load(json_file)
+                content.append(to_json)
+            with open("contacts_data.json", "w") as json_file:
+                json.dump(content, json_file, ensure_ascii=False, indent=2)
+        except FileNotFoundError:
+            with open("contacts_data.json", "w", encoding='utf-8') as json_file:
+                content = [to_json]
+                json.dump(content, json_file)
     return render(request, 'catalog/contacts.html')
